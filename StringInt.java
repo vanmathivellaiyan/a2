c3c57ba6ed133540895762953c38b14c33c484bd
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-29 10:17:00 +0530

    Add files via upload

---
+++ StringInt.java
@@ -0,0 +1,9 @@
+package logicbuilding;
+
+public class StringInt {
+	public static void main(String[] args){
+	String numberAsString = "1234";
+	int number = Integer.parseInt(numberAsString);
+	System.out.println("The number is: " + number);
+}
+}
